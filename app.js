a5d4a40a21f6ae312dcab0aa246770b2635c00c5
renodubois <rtdm9c@mst.edu>
2017-02-14 20:14:16 -0600

    Changed 'var' to 'let'
    More accurately follows EMCA6 standards.

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use("/users", routers.users.router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
