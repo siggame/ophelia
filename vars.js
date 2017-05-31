38f727010fd69f65c889c9e30a43003f45810740
Reno DuBois <renodubois@users.noreply.github.com>
2017-05-30 21:40:25 -0500

    Removed GitLab vars

--- vars.js
+++ vars.js
@@ -3,8 +3,6 @@
 const _ = require('lodash');
 
 module.exports = {
-    GITLAB_HOST : _.defaultTo(process.env.GITLAB_HOST, "localhost:80"),
-    GITLAB_SECRET : _.defaultTo(process.env.GITLAB_SECRET, "GITLAB_SECRET"),
     HOST : _.defaultTo(process.env.HOST, "localhost"),
     PORT : _.defaultTo(process.env.PORT, 3000),
     DB_HOST : _.defaultTo(process.env.DB_HOST, "localhost"),
