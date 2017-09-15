ba29edbef9dd75ff5bc90d2f5230a72b96f558b9
Erik Shively <ErikShively@users.noreply.github.com>
2017-09-14 22:23:06 -0500

    Initial commit w/ barebones files

---
+++ client/src/components/login/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Login</h1>
+                <p>put stuff here!</p>
+            </div>
+        )
+    }
+
+}
