ca49924cfc619511655c61c914f93de924f46beb
Dillon Hess <dnhc59@mst.edu>
2017-09-24 19:11:12 -0500

    Created getSubmissionByTeamName/cleaned up code

--- db/teams.js
+++ db/teams.js
@@ -23,12 +23,7 @@ function getTeam(teamId) {
     });
 }
 
-
-//Test get submission
-
-
-//This needs to use the teamName to get the team id from teams table. Then use that id to get the submissions by
-//using team_id as the key
+//Gets the team info by using the teamName that is passed to it
 function getSubmissionByTeamName(teamName) {
     return new Promise((resolve, reject) => {
         knex.select('*').from('submissions').joinRaw('natural full join teams').where('name',teamName)
@@ -72,59 +67,3 @@ module.exports = {
     editTeam: editTeam,
     getSubmission: getSubmissionByTeamName
 };
-
-
-
-// console.log('a')
-// function stuff(stuff) {
-//     return new Promise((resolve, reject) => {
-//         // Do some code...
-//         //If something went well
-//         const print = 'c'
-//         console.log('b')
-//         return resolve(print);
-//         if(1 == "1") {
-//             return reject()
-//         }
-//     })
-// }
-// stuff.then((stuff) => {
-//     console.log(stuff)
-// }, () => {
-//     console.log('total failure')
-// }).catch(() => {
-//     // SOEIFNSEOIGHSOEIGNSEOIUGBSEOIGbOSEIGBOESIB
-// })
-// console.log('c')
-
-
-
-
-// function getSubmission(teamID) {
-//     return new Promise(resolve, reject) {
-//         //...do stuff here
-//         //.soieufbsieufbiseubfse
-//         knex.select({
-//             name: 'team1',
-//             contact_email: 'team@123.nowhere',
-//             password: 'plaintext_password!',
-//             is_eligible: 'true',
-//             created_at: Date.now(),
-//             updated_at: Date.now()
-//         }).then((rows) => {
-//             //woo hoo
-//             return resolve(rows)
-//         }).catch((err) => {
-//             return reject(err)
-//         })
-//
-//
-//     }
-// }
-
-getSubmissionByTeamName('bestteam').then((res) => {
-    console.log(res)
-}, (err) => {
-    console.log('TOTAL FAILURE', err)
-}).catch(() => {
-});
