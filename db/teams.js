bc244c5115d853741925b67a9e939415a30bd086
Reno DuBois <renodubois@users.noreply.github.com>
2017-09-26 15:08:07 -0500

    TG-36 Ran standard --fix

--- db/teams.js
+++ db/teams.js
@@ -1,38 +1,38 @@
-"use strict";
+'use strict'
 
 const knex = require('knex')({
-    client: 'pg',
-    connection: {
-        host: 'localhost',
-        port: '5432',
-        user: 'postgres',
-        password: 'siggame',
-        database: 'postgres'
-    }
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    port: '5432',
+    user: 'postgres',
+    password: 'siggame',
+    database: 'postgres'
+  }
 })
 
-function getTeam(teamId) {
-    return new Promise((resolve, reject) => {
-        knex('team').where({
-            id: teamId
-        }).then((res)=>{
-            resolve(res);
-        }).catch((err)=>{
-            reject(err);
-        })
-    });
+function getTeam (teamId) {
+  return new Promise((resolve, reject) => {
+    knex('team').where({
+      id: teamId
+    }).then((res) => {
+      resolve(res)
+    }).catch((err) => {
+      reject(err)
+    })
+  })
 }
 
-function getTeamByName(teamName) {
-    return new Promise((resolve, reject) => {
-        knex('teams').where({
-            name: teamName
-        }).then((res) => {
-            resolve(res);
-        }).catch((res) => {
-            reject(res);
-        })
-    });
+function getTeamByName (teamName) {
+  return new Promise((resolve, reject) => {
+    knex('teams').where({
+      name: teamName
+    }).then((res) => {
+      resolve(res)
+    }).catch((res) => {
+      reject(res)
+    })
+  })
 }
 
 /*
@@ -52,17 +52,17 @@ function getAllTeamNames () {
   })
 }
 
-function editTeam(teamData) {
+function editTeam (teamData) {
     // TODO: Some sort of checking on teamData
-    return new Promise((resolve, reject) => {
-        knex('team').where({
-            id: teamData.id
-        }).update(teamData).then((res) => {
-            resolve(res);
-        }).catch((err) => {
-            reject(err);
-        })
+  return new Promise((resolve, reject) => {
+    knex('team').where({
+      id: teamData.id
+    }).update(teamData).then((res) => {
+      resolve(res)
+    }).catch((err) => {
+      reject(err)
     })
+  })
 }
 
 module.exports = {
