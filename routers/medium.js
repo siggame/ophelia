50017da58b2221b6dd0c877b692b813c32a94291
Reno DuBois <rdubois96@gmail.com>
2017-10-05 13:24:07 -0500

    consolidated to /medium/, grabbing from correct URL

--- routers/medium.js
+++ routers/medium.js
@@ -4,8 +4,12 @@ const feed = require('rss-to-json')
 
 const path = '/medium'
 
+/**
+ * GET '/medium/' - gets the 5 newest posts from the SIG-Game Medium page.
+ * Posts include: title, desc, link, url, created
+ */
 router.get(path + '/', (req, res) => {
-  feed.load('https://www.medium.com/feed/@siggame', (err, rss) => {
+  feed.load('https://www.medium.com/feed/siggame', (err, rss) => {
     if (err) {
       res.status(500).json({
         success: false,
@@ -13,23 +17,7 @@ router.get(path + '/', (req, res) => {
         data: []
       })
     } else {
-      res.status(200).json({
-        success: true,
-        data: rss.items
-      })
-    }
-  })
-})
-
-router.get(path + '/latest', (req, res) => {
-  feed.load('https://www.medium.com/feed/@siggame', (err, rss) => {
-    if (err) {
-      res.status(500).json({
-        success: false,
-        err: err,
-        data: []
-      })
-    } else {
+      console.log(rss)
       res.status(200).json({
         success: true,
         data: rss.items.slice(0, 5)
