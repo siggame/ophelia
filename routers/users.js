661fa3eb25755823747dab267ca44e057a4a76c8
Michael Beaver <michaelbeaver64@gmail.com>
2017-10-04 19:03:57 -0500

    TG-41 added some basic starting information to call the db function

--- routers/users.js
+++ routers/users.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
-// const db = require('../db/init')
+const db = require('../db/init')
 
 // All paths in this file should start with this
 const path = '/users'
@@ -16,7 +16,27 @@ router.post(path + '/', (req, res) => {
 })
 
 router.get(path + '/:teamName', (req, res) => {
-  res.send('teamName is set to ' + req.params.teamName)
+  const response = {
+    success: false,
+    message: '',
+    user: {
+      name: null,
+      contactEmail: null,
+      isEligible: null
+    }
+  }
+  db.teams.getTeamByName(req.params.teamName).then((data) => {
+    response.success = true
+    // TODO Needs to actually extract/validate the data
+    response.user = data
+    res.json(response)
+  }, (err) => {
+    // TODO Figure out how to properly make this a string?
+    response.message = err
+    response.user = null
+    res.json(response)
+  })
+  // TODO Needs a .catch() to handle exceptions
 })
 
 router.put(path + '/:teamName', (req, res) => {
