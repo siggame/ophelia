bedef8d82746896b39b2e9e0cb4d93cadeb5cec3
gideonw14 <gideonw14@gmail.com>
2017-10-08 20:25:12 -0500

    TG-32 right now the getGame function is just a slightly modified version of getSubmissionByTeamName that only gets the most recent version number.

--- db/teams.js
+++ db/teams.js
@@ -115,6 +115,55 @@ function createTeam (teamName, email, password, isEligible) {
   })
 }
 
+function getGame (teamName) {
+  return new Promise((resolve, reject) => {
+    if (teamName === null || typeof teamName === 'undefined') {
+      reject(new Error('TeamName is null or undefined'))
+    }
+    knex.select('id')
+      .from('submissions')
+      .joinRaw('natural full join teams', function () {
+      this.on('teams.id', '=', 'submissions.team_id')
+        .onIn('teams.name', teamName)
+      })
+      .where('version', '=', knex('submissions').max('version'))
+      .then((res) => {
+        for (let row of res) {
+          delete row['password']
+        }
+        return resolve(res)
+      }).catch((err) => {
+      return reject(err)
+    })
+  })
+}
+
+// function getGame (teamName) {
+//   return new Promise((resolve, reject) => {
+//     if (teamName === null || typeof teamName === 'undefined') {
+//       reject(new Error('TeamName is null or undefined'))
+//     }
+//     knex.select('id')
+//       .from('games')
+//       .joinRaw('natural full join games_submissions', function () {
+//         this.on('games.id', '=', 'games_submissions.game_id')
+//       })
+//       .joinRaw('natural full join submissions', function() {
+//         this.on('submissions.id', '=', 'games_submissions.submissions_id')
+//           .onIn('submissions.version', knex('submissions').max('version'))
+//       })
+//       .joinRaw('natural full join teams', function () {
+//         this.on('teams.id', '=', 'submissions.team_id')
+//           .onIn('teams.name', teamName)
+//       })
+//       .then((res) => {
+//         return resolve(res)
+//       }).catch((err) => {
+//       return reject(err)
+//     })
+//   })
+// }
+
 module.exports = {
   createTeam: createTeam,
   getTeam: getTeam,
@@ -123,11 +172,21 @@ module.exports = {
   getSubmissionByTeamName: getSubmissionByTeamName
 }
 
-getSubmissionByTeamName('testTeam').then((res) => {
-  console.log(res)
-}, (err) => {
-  console.log(err)
-}
+getGame('team1').then((res) => {
+    console.log('I got the team sub')
+    console.log(res)
+  }, (err) => {
+    console.log(err)
+  }
 ).catch((err) => {
   console.log(err)
 })
+
+// getSubmissionByTeamName('testTeam').then((res) => {
+//   console.log(res)
+// }, (err) => {
+//   console.log(err)
+// }
+// ).catch((err) => {
+//   console.log(err)
+// })
