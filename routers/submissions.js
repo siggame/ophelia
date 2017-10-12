23e654b30ff4af82db20c9f65e5a2253982c29e8
Rotheron <lilbing99@gmail.com>
2017-10-11 19:32:12 -0500

    TG-45 Successfully returning the response messages and status codes.

--- routers/submissions.js
+++ routers/submissions.js
@@ -2,13 +2,30 @@
 
 const express = require('express')
 const router = express.Router()
-// const db = require('../db/init')
+const db = require('../db/init')
 
 // All paths in this file should start with this
 const path = '/submissions'
 
 router.get(path + '/', (req, res) => {
-
+  const user = 'bob'
+  const response = {
+    success: false,
+    message: '',
+    submissions: null
+  }
+  db.teams.getSubmissionByTeamName(user).then((result) =>{
+    response.success = true
+    response.message = "Data successfully retrieved: "
+    response.submissions = result
+    res.status(200).json(response)
+  }, (err) => {
+    response.message = err.toString()
+    res.status(500).json(response)
+  }).catch((err) => {
+    response.message = "An error occured: " + err
+    res.status(500).json(err)
+  })
 })
 
 router.post(path + '/', (req, res) => {
