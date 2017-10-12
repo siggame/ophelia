087e2f99abee59b1ac03dbead2e28dec791ff48f
Reno DuBois <rdubois96@gmail.com>
2017-10-11 21:34:35 -0500

    Update main routes

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-<<<<<<< HEAD
-import NavBar from "../components/common/navbar";
-import LandingPage from "../components/LandingPage";
-import Dashboard from "./dashboard/dashboard";
-import Profile from "./profile/profile";
-import Login from "../components/login/login";
-=======
-import NavBar from '../components/common/navbar'
+import NavBar from '../components/NavBar'
 import LandingPage from '../components/LandingPage'
 import Dashboard from './dashboard/dashboard'
 import Profile from './profile/profile'
-import Signup from '../components/signup/signup'
->>>>>>> origin/develop
+import Signup from '../containers/Signup'
+import Login from '../containers/Login'
 
 export default class App extends React.Component {
   render () {
@@ -22,22 +15,16 @@ export default class App extends React.Component {
         <NavBar />
         <main>
           <Switch>
-<<<<<<< HEAD
-            <Route exact path="/" component={LandingPage} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route path="/profile/:teamId" component={Profile} />
-            <Route exact path="/login" component={Login} />
-=======
             <Route exact path='/' component={LandingPage} />
             <Route exact path='/dashboard' component={Dashboard} />
             <Route path='/profile/:teamId' component={Profile} />
-            <Route exact path='/signup' component={Signup} />
->>>>>>> origin/develop
+            <Route exact path='/register' component={Signup} />
+            <Route exact path='/login' component={Login} />
           </Switch>
         </main>
 
         <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js' />
-        <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous' />
+        <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossOrigin='anonymous' />
       </div>
     )
   }
