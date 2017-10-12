5b89ec70feaaab05b8ce364ba8273836d6064b76
Gideon Walker <gideonw14@gmail.com>
2017-10-11 22:05:25 -0500

    TG-48 The beginings of GET /games, its very ugly right now.

--- routers/games.js
+++ routers/games.js
@@ -2,13 +2,35 @@
 
 const express = require('express')
 const router = express.Router()
-// const db = require('../db/init')
+const db = require('../db/init')
 
 // All paths in this file should start with this
 const path = '/games'
 
 router.get(path + '/', (req, res) => {
-
+	var teamName = 'fix me'
+	var games = [];
+	var success
+	var message = ''
+	// if user is auth'ed
+	//	get his teamName
+	db.teams.getGame(teamName).then((res) => {
+		// maybe check if res is empty
+	    games = res
+	   	success = 200
+	   	message = 'OK'
+	  }, (err) => {
+	    success = 500
+	 	message = 'Unknown internal error'
+	  }
+	).catch((err) => {
+	  success = 500
+	  message = 'Unknown internal error'
+	})
+	// else
+	//	success = 401
+	// 	message = 'Not authorized'
+	return {success, message, games} // not sure if this is the right syntax
 })
 
 router.get(path + '/:gameID', (req, res) => {
