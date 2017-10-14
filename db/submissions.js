d7792101ab31a8ddbc0ab894cc45a751cd8821e2
Michael Beaver <michaelbeaver64@gmail.com>
2017-10-14 18:10:32 -0500

    moved submissions function to its own file

---
+++ db/submissions.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/**
+ * Joins teams and submissions tables together and returns resulting rows that
+ * include the given name
+ * @param teamName name of the team from the 'teams' table
+ * @return {Promise} resolves with a list of submissions under that team name
+ */
+function getSubmissionByTeamName (teamName) {
+  return new Promise((resolve, reject) => {
+    if (teamName === null || typeof teamName === 'undefined') {
+      reject(new Error('TeamName is null or undefined'))
+    }
+    knex('submissions')
+      .join('teams', 'teams.id', '=', 'submissions.team_id')
+      .select('version', 'status', 'submission_url', 'log_url', 'image_name')
+      .where('teams.name', '=', teamName)
+      .then((res) => {
+        return resolve(res)
+      }).catch((err) => {
+      return reject(err)
+    })
+  })
+}
+
+module.exports = {
+  getSubmissionByTeamName
+}
--- db/teams.js
+++ db/teams.js
@@ -36,29 +36,6 @@ function getAllTeamNames () {
   })
 }
 
-/**
- * Joins teams and submissions tables together and returns resulting rows that
- * include the given name
- * @param teamName name of the team from the 'teams' table
- * @return {Promise} resolves with a list of submissions under that team name
- */
-function getSubmissionByTeamName (teamName) {
-  return new Promise((resolve, reject) => {
-    if (teamName === null || typeof teamName === 'undefined') {
-      reject(new Error('TeamName is null or undefined'))
-    }
-    knex('submissions')
-      .join('teams', 'teams.id', '=', 'submissions.team_id')
-      .select('version', 'status', 'submission_url', 'log_url', 'image_name')
-      .where('teams.name', '=', teamName)
-      .then((res) => {
-        return resolve(res)
-      }).catch((err) => {
-        return reject(err)
-      })
-  })
-}
-
 function getTeamByName (teamName) {
   return new Promise((resolve, reject) => {
     knex('teams').where({
@@ -152,7 +129,6 @@ module.exports = {
   getTeam: getTeam,
   getTeamByName: getTeamByName,
   editTeam: editTeam,
-  getSubmissionByTeamName: getSubmissionByTeamName,
   getGame: getGame,
   getAllTeamNames: getAllTeamNames
 }
