36bb1ba47884f005f0a92576b3b61b1f40cdde90
Chad Van Horn <vanhornchadm@gmail.com>
2017-10-15 18:09:37 -0500

    Completed ticket

--- routers/users.js
+++ routers/users.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
-// const db = require('../db/init')
+const db = require('../db/init')
 
 // All paths in this file should start with this
 const path = '/users'
@@ -20,7 +20,35 @@ router.get(path + '/:teamName', (req, res) => {
 })
 
 router.put(path + '/:teamName', (req, res) => {
-  res.send('teamName is set to ' + req.params.teamName)
+  const response = {
+    success: false,
+    message: ''
+  }
+  const userdata = req.body
+  let data = {
+    'contact_email': req.body['contactEmail'],
+    'password': req.body['password']
+  }
+  db.teams.getTeamByName(req.params.teamName).then((result)=>{
+    data['id'] = result[0].id
+    db.teams.editTeam(data).then((result) =>{
+      response.success = true
+      response.message = 'Edited user successfully'
+      res.status(200).json(response)
+    },(err) => {
+      response.message = err.message
+      res.status(400).json(response)
+    }).catch((err) => {
+      response.message = err.message
+      res.status(500).json(response)
+    })
+  },(err) => {
+    response.message = err.message
+    res.status(400).json(response)
+  }).catch((err) => {
+    response.message = err.message
+    res.status(500).json(response)
+  })  
 })
 
 module.exports = {router}
