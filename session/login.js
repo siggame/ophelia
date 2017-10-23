75043a18d3748bce714b306be1fc160aeae914b3
Michael Beaver <michaelbeaver64@gmail.com>
2017-10-22 19:31:35 -0500

    TG-42 Modified log in function so it responds with an object instead of two variables

--- session/login.js
+++ session/login.js
@@ -8,15 +8,19 @@ const compare = require('./auth').compare
  * @param teamName - String -  The name of the team who we are trying to log in
  * @param password - String - The password of the team who is trying to log in
  * @return {Promise}
- *  resolve - (LoggedIn - Boolean, Role - String) - If success, passes role
+ *  resolve - ({ success: Boolean, role: String}) - If success, passes role
  *  reject - (err) - Rejects if there is an error
  */
 function login (teamName, password) {
+  const response = {
+    success: false,
+    role: ''
+  }
   return new Promise((resolve, reject) => {
     teams.getTeamByName(teamName).then((team) => {
       if (typeof team === 'undefined') {
         // If the team is undefined then there must not have been a match
-        return resolve(false, null)
+        return resolve(response)
       } else {
         const encryptedPassword = team.password
         const salt = team.salt
@@ -24,10 +28,12 @@ function login (teamName, password) {
         const role = team.role
         // Checking to see if given password matches the one in the db
         if (compare(encryptedPassword, password, salt, iterations)) {
-          return resolve(true, role)
+          response.success = true
+          response.role = role
+          return resolve(response)
         }
         // If compare failed then they must have given the wrong password
-        return resolve(false, null)
+        return resolve(response)
       }
     }, (err) => {
       reject(err)
