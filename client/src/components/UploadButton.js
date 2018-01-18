3083e1d34b3aff375e5e180743abe24cdf56463d
Reno DuBois <rdubois96@gmail.com>
2018-01-17 21:28:56 -0600

    WIP of submission

---
+++ client/src/components/UploadButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class UploadButton extends React.Component {
+  render () {
+    return (
+      <div>
+        Upload AI (.zip)
+      </div>
+    )
+  }
+}
--- client/src/components/dashboard/SubmissionInfo.js
+++ client/src/components/dashboard/SubmissionInfo.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import UploadButton from '../UploadButton'
 
 export default class SubmissionInfo extends React.Component {
   render () {
+    const statusStyle = {}
+    const uploadedTime = ''
+    const uploadedDate = this.props.uploadedDate
+
     return (
       <div>
-        <p>Replace me!</p>
+        <h1>Latest Submission:</h1>
+        <div style={{ marginLeft: 10 }} >
+          <p>
+            <span>Uploaded:</span> ({uploadedTime}) {uploadedDate}
+          </p>
+          <p>
+            Status: <span style={statusStyle} >{this.props.status}</span>
+          </p>
+          <UploadButton />
+        </div>
       </div>
     )
   }
+}
+
+SubmissionInfo.defaultProps = {
+  uploadedDate: '',
+
 }
