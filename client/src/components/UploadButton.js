41689e47c1de8f161a5939055510b51e5bfbdeee
Reno DuBois <rdubois96@gmail.com>
2018-02-05 00:48:54 -0600

    Initial UploadButton changes

--- client/src/components/UploadButton.js
+++ client/src/components/UploadButton.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import { Uploader, UploadField } from '@navjobs/upload'
 
 export default class UploadButton extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      file: null
+    }
+
+    this.uploadFile = this.uploadFile.bind(this)
+  }
+
   render () {
+    // TODO: Handle when uploads are complete
     return (
-      <div className='btn btn-primary submission-button' >
-        Upload AI (.zip)
-      </div>
+      <Uploader
+        request={{
+          fileName: 'file',
+          url: '/submissions/',
+          method: 'POST'
+        }}
+        onComplete={() => console.log('yay!')}
+        uploadOnSelection
+      >
+        {({ onFiles, progress, complete }) => (
+          <div>
+            <UploadField
+              onFiles={onFiles}
+              uploadProps={{
+                accept: '.zip'
+              }}
+            >
+              <div className='btn btn-primary'>Upload AI (.zip)</div>
+            </UploadField>
+            {progress ? `Progress: ${progress}` : null}
+            {complete ? 'Complete!' : null}
+          </div>
+        )}
+      </Uploader>
     )
   }
 }
