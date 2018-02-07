194720a38fc3b534522d17c1c895d358e62fc822
Eric Willard <emwwwc@m.st.edu>
2018-02-07 16:23:13 -0600

    Created basic input validation functions and added checking for input in the login.js

---
+++ utils/sanitizer.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const validator = require('validator')
+
+const usernameRegex = /^[a-zA-Z0-9.-]+$/
+const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]*$/
+const emailRegex = /^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]$/
+
+const emailMinLength = 5
+const emailMaxLength = 100
+const usernameMinLength = 4
+const usernameMaxLength = 50
+const passwordMinLength = 8
+const passwordMaxLength = 50
+
+/**
+ * Function to check if the length of a string in certain range
+ * @param str String to check
+ * @param minLen Minimum possible length of the string
+ * @param maxLen Maximum possible length of the string
+ * @returns {boolean}
+ */
+function checkLength (str, minLen, maxLen) {
+  return !!validator.isLength(str, {min: minLen, max: maxLen})
+}
+
+/**
+ * Function to check if a email is valid
+ * @param email Email to check
+ * @returns {boolean}
+ */
+function isValidEmail (email) {
+  return !!(checkLength(email, emailMinLength, emailMaxLength) && validator.matches(email, emailRegex))
+}
+
+/**
+ * Function to check if a username is valid
+ * @param username Username to check
+ * @returns {boolean}
+ */
+function isValidUsername (username) {
+  return !!(checkLength(username, usernameMinLength, usernameMaxLength) && validator.matches(username, usernameRegex))
+}
+
+/**
+ * Function to check if a password is valid
+ * @param password Password to check
+ * @returns {boolean}
+ */
+function isValidPassword (password) {
+  return !!(checkLength(password, passwordMinLength, passwordMaxLength) && validator.matches(password, passwordRegex))
+}
+
+/**
+ * Function to check if a username and password combination is correct
+ * @param username Username to check
+ * @param password Password to check
+ * @returns {boolean}
+ */
+function isValidLogin (username, password) {
+  return !!(isValidUsername(username) && isValidPassword(password))
+}
+
+module.exports = {
+  isValidEmail,
+  isValidUsername,
+  isValidPassword,
+  isValidLogin
+}
