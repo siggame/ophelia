52346594f831327db1d0f7673f3ce6fbe0a34bd7
Michael Beaver <michaelbeaver64@gmail.com>
2018-02-10 13:54:26 -0600

    Cleaned up login function and made returns make more sense

--- session/login.js
+++ session/login.js
@@ -8,32 +8,36 @@ const compare = require('./auth').compare
  * @param teamName - String -  The name of the team who we are trying to log in
  * @param password - String - The password of the team who is trying to log in
  * @return {Promise}
- *  resolve - ({ success: Boolean, role: String}) - If success, passes role
+ *  resolve - (Boolean || User Info Object) -
+ *      User Object if user successfully logs in
+ *        {
+ *          id: row ID,
+ *          role: String
+ *        }
+ *      false if it worked, but password is wrong
+ *      null if the user doesn't exist
  *  reject - (err) - Rejects if there is an error
  */
 function login (teamName, password) {
-  const response = {
-    success: false,
-    role: ''
-  }
   return new Promise((resolve, reject) => {
     teams.getTeamByName(teamName).then((team) => {
       if (typeof team === 'undefined') {
         // If the team is undefined then there must not have been a match
-        return resolve(response)
+        return resolve(null)
       } else {
         const encryptedPassword = team.password
         const salt = team.salt
         const iterations = team.hash_iterations
-        const role = team.role
         // Checking to see if given password matches the one in the db
         if (compare(encryptedPassword, password, salt, iterations)) {
-          response.success = true
-          response.role = role
-          return resolve(response)
+          const userInfo = {
+            id: team._id,
+            role: team.role
+          }
+          return resolve(userInfo)
         }
         // If compare failed then they must have given the wrong password
-        return resolve(response)
+        return resolve(false)
       }
     }, (err) => {
       reject(err)
