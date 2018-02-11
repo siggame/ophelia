fdbb31469ef675d4c71be239ed832a7ca7e1afff
Reno DuBois <rdubois96@gmail.com>
2018-02-11 15:09:45 -0600

    Update AuthStore to use JWT

--- client/src/stores/auth.js
+++ client/src/stores/auth.js
@@ -1,26 +1,44 @@
 import axios from 'axios'
-import { extendObservable } from 'mobx'
+import { action, computed, observable, reaction } from 'mobx'
 
-export default class AuthStore {
+export class AuthStore {
+  @observable username = ''
+  @observable token = window.localStorage.getItem('jwt')
+
   constructor () {
-    extendObservable(this, {
-      token: '',
-      username: '',
-      get isUserLoggedIn () {
-        return !!(this.username && this.token)
+    // Updates or removes our JSON Web Token in the localStorage of the browser.1
+    reaction(
+      () => this.token,
+      token => {
+        if (token) {
+          window.localStorage.setItem('jwt', token)
+        } else {
+          window.localStorage.removeItem('jwt')
+        }
       }
-    })
+    )
   }
 
-  logUserIn (username, password) {
-    return new Promise((resolve, reject) => {
-      axios.post('/login',
+  @computed get isUserLoggedIn () {
+    return !!(this.token)
+  }
+
+  @action setToken(token) {
+    this.token = token
+  }
+
+  @action setUsername(username) {
+    this.username = username
+  }
+
+  @action logUserIn (username, password) {
+    return new Promise(action('login-callback', (resolve, reject) => {      axios.post('/login',
         {
           username: username,
           password: password
         }
       ).then((response) => {
-        this.token = response.data.token
+        this.setToken(response.data.token)
         this.username = username
         return resolve()
       }).catch((err) => {
@@ -28,11 +46,13 @@ export default class AuthStore {
         console.log('Axios Error - Auth Store')
         return reject(err)
       })
-    })
+    }))
   }
 
-  logUserOut () {
+  @action logUserOut () {
     this.username = ''
     this.token = ''
   }
 }
+
+export default new AuthStore()
