e6becc91b14094f357fadbd89bda583531a3c72b
Michael Beaver <michaelbeaver64@gmail.com>
2018-02-16 16:55:13 -0600

    Finishing changes to merge in develop

--- server/vars.js
+++ server/vars.js
@@ -12,7 +12,7 @@ module.exports = {
   HOST: _.defaultTo(process.env.HOST, 'localhost'),
   PORT: _.defaultTo(process.env.PORT, 3000),
   PASSWORD_SECRET: _.defaultTo(process.env.PASSWORD_SECRET, 'secret'),
-  SENTRY_KEY: _.defaultTo(process.env.SENTRY_KEY, 'null')
+  SENTRY_KEY: _.defaultTo(process.env.SENTRY_KEY, 'null'),
   TOKEN_SECRET: _.defaultTo(process.env.TOKEN_SECRET, 'token_secret'),
   TOKEN_EXPIRE_TIME: _.defaultTo(process.env.TOKEN_EXPIRE_TIME, '1d')
 }
