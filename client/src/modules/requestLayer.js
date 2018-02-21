487e65ca191700db0f7e3c33c0be48eede338772
Reno DuBois <renodubois@users.noreply.github.com>
2018-02-20 22:07:07 -0600

    Add function to upload submissions

--- client/src/modules/requestLayer.js
+++ client/src/modules/requestLayer.js
@@ -28,18 +28,39 @@ export default class RequestLayer {
     return new Promise((resolve, reject) => {
       // Check first to make sure the user is logged in
       if (!stores.authStore.isUserLoggedIn) {
-        return reject(new Error('User must be logged in to fetch games'))
+        return reject(new Error('User must be logged in to fetch submissions'))
       }
       axios.get('/submissions/', {
         headers: {
           Authorization: `Bearer ${stores.authStore.token}`
-        },
-        params: {
-          page: 1
         }
       }).then((response) => {
         return resolve(response.data.submissions)
       }).catch((err) => {
+        return reject(err)
+      })
+    })
+  }
+
+  uploadSubmissions (file) {
+    return new Promise((resolve, reject) => {
+      if (!stores.authStore.isUserLoggedIn) {
+        return reject(new Error('User must be logged in to upload submissions'))
+      }
+      let formData = new FormData()
+      formData.append('file', file)
+      axios.post('/submissions/',
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${stores.authStore.token}`,
+            'Content-Type': 'multipart/form-data'
+        }
+      }).then((result) => {
+        console.log('Got a result, neat')
+        return resolve(result)
+      }).catch((err) => {
+        // TODO: Log this happening (means Arena failed us basically)
         return reject(err)
       })
     })
