ea2ff115ee1bf0ee0e57566fba16e05e9da6058e
Reno DuBois <renodubois@users.noreply.github.com>
2018-02-20 22:01:19 -0600

    Add UploadButton for Submissions

--- client/src/components/UploadButton.js
+++ client/src/components/UploadButton.js
@@ -1,53 +1,44 @@
 import { inject } from 'mobx-react'
 import React from 'react'
-import { Uploader, UploadField } from '@navjobs/upload'
+import { UploadField } from '@navjobs/upload'
 
-@inject('authStore')
+@inject('submissionStore')
 export default class UploadButton extends React.Component {
   constructor (props) {
     super(props)
-
     this.state = {
       file: null
     }
-
     this.uploadFile = this.uploadFile.bind(this)
   }
 
-  uploadFile () {
-    return
+  uploadFile (file) {
+    const acceptedFiletypes = [
+      'application/zip',
+      'application/gzip',
+      'application/x-tar'
+    ]
+    if (acceptedFiletypes.includes(file.type)) {
+      this.props.submissionStore.uploadSubmission(file)
+    } else {
+      this.props.submissionStore.uploadError = 'Incorrect filetype - file must be .zip, .tar or .tar.gz.'
+    }
   }
 
   render () {
-    // TODO: Handle when uploads are complete
     return (
-      <Uploader
-        request={{
-          fileName: 'file',
-          headers: {
-            Authorization: `Bearer ${this.props.authStore.token}`
-          },
-          url: '/submissions/',
-          method: 'POST'
-        }}
-        onComplete={() => console.log('yay!')}
-        uploadOnSelection
-      >
-        {({ onFiles, progress, complete }) => (
-          <div>
-            <UploadField
-              onFiles={onFiles}
-              uploadProps={{
-                accept: '.zip,.tar,.tar.gz'
-              }}
-            >
-              <div className='btn btn-primary'>Upload AI (.zip, .tar, .tar.gz)</div>
-            </UploadField>
-            {progress ? `Progress: ${progress}` : null}
-            {complete ? 'Complete!' : null}
+      <div className='col-sm-4'>
+        <UploadField
+          onFiles={files => this.uploadFile(files[0])}
+          uploadProps={{
+            accept: '.zip,.tar,.tar.gz'
+          }}
+        >
+          <div style={{ width: '100%' }} className='btn btn-primary'>
+            Upload AI
           </div>
-        )}
-      </Uploader>
+        </UploadField>
+      </div>
     )
   }
 }
