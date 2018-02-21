1db5cdd04ea9c0c100683b7a6747c3abed3beca1
Reno DuBois <renodubois@users.noreply.github.com>
2018-02-20 22:03:06 -0600

    Make Dashboard show SubmissionContainer instead of Component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import Profile from '../profile/Profile'
 import GamesContainer from '../../containers/GamesContainer'
-import SubmissionInfo from './SubmissionInfo'
+import SubmissionContainer from '../../containers/SubmissionContainer'
 // import SubmissionBanner from './SubmissionBanner'
 
 export default class Dashboard extends React.Component {
   render () {
     return (
 		<div>
-		  {/* <SubmissionBanner uploadedDate={'2018-01-10T12:00:05'} /> */}
 		  <div className='row'>
 			  <div className='col-lg-6'>
 			    <GamesContainer />
 			  </div>
 			  <div className='col-lg-6' >
 			    <div className='submission'>
-				    <SubmissionInfo uploadedDate={'2018-01-10T12:00:05'} />
+				    <SubmissionContainer />
 			    </div>
 			    <div className='profile'>
 				    <Profile />
