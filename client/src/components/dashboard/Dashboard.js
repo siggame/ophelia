11c91c395dee9a79f4fdc830f88ecddee94e182a
cmparsons <christianp130@gmail.com>
2018-02-21 21:36:38 -0600

    Redo profile component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,18 +4,21 @@ import GamesContainer from '../../containers/GamesContainer'
 import SubmissionInfo from './SubmissionInfo'
 
 export default class Dashboard extends React.Component {
-  render () {
+  render() {
+    const bio = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum elementum elit ac vestibulum sodales. Mauris tempor consectetur mi nec tempor. Etiam fermentum maximus lacus, quis interdum ligula lacinia ac. Maecenas sagittis metus quis sem feugiat ultricies. Praesent imperdiet tincidunt metus at mollis. Ut sit amet libero id urna accumsan varius at aliquet nulla. Fusce tortor orci, rhoncus interdum velit eget, pellentesque ullamcorper diam. Pellentesque sed vestibulum mauris. Suspendisse neque odio, rhoncus eget tempor eget, tempus quis nisl. Donec hendrerit, lorem non lacinia ultrices, erat odio rhoncus tortor, eu posuere dolor metus sed eros. Pellentesque eget nisi posuere dolor luctus vestibulum. Duis aliquam convallis est nec interdum. Sed nibh mi, tempor ut nunc id, luctus tincidunt ex. Vestibulum nec velit hendrerit nulla mollis laoreet et sit amet tellus. Nunc non fringilla mi. Duis arcu mi, congue pharetra lorem eu, pulvinar ultrices sapien.'
     return (
-      <div className='row'>
-        <div className='col-lg-6'>
-          <GamesContainer />
-        </div>
-        <div className='col-lg-6' >
-          <div className='submission'>
-            <SubmissionInfo />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-6'>
+            <GamesContainer />
           </div>
-          <div className='profile'>
-            <Profile />
+          <div className='col-lg-6' >
+            <div className='submission'>
+              <SubmissionInfo />
+            </div>
+            <div className='profile'>
+              <Profile username={'dummyUser'} teamName={'dummyTeamName'} bio={bio} imageUrl={''} />
+            </div>
           </div>
         </div>
       </div>
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,22 +1,48 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default class Profile extends React.Component {
-  render () {
-    const styleBold = {
-      fontWeight: 'bold'
-    }
-    return (
-      <div>
-        <h1>{this.props.teamName} </h1>
-        <p style={styleBold}> {this.props.primaryContactName} </p>
-        <p><span style={styleBold}>Email: </span>{this.props.primaryContactEmail} </p>
+const Profile = ({ username, teamName, bio, imageUrl }) => (
+  <div>
+    <div className='row'>
+      <div className='col-md-4 col-md-offset-4'>
+        <div className='page-header'>
+          <h1>{username}</h1>
+        </div>
       </div>
-    )
-  }
-}
+    </div>
+    <div className='row'>
+      <div className='col-md-4'>
+        <div className='thumbnail'>
+          <img src={imageUrl} alt={username} />
+        </div>
+      </div>
+      <div className='col-md-8'>
+        <dl className='dl-horizontal'>
+          <dt>Team Name</dt>
+          <dd>
+            {teamName === '' ? `${username} is currently a F/A` : teamName}
+          </dd>
+        </dl>
+        <dl className='dl-horizontal'>
+          <dt>Bio</dt>
+          <dd style={{overflowY: 'auto', maxHeight: 200, width: 'auto' }}>{bio}</dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+)
 
 Profile.defaultProps = {
   teamName: '',
-  primaryContactName: '',
-  primaryContactEmail: ''
+  bio: '',
+  imageUrl: '' // TODO: Should we have a default image?
 }
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  teamName: PropTypes.string,
+  bio: PropTypes.string,
+  imageUrl: PropTypes.string
+}
+
+export default Profile
