adb1cd2b3afc1882dcf8b66ac150471777458bbc
Dillon Hess <dnhc59@mst.edu>
2018-02-23 17:55:51 -0600

    Added another test login button click doesn't work

---
+++ tests/clickOnLogin.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'Click Login': function (browser) {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('.login-button', 3000)
+      .click('.login-button')
+      .pause(3000)
+      .assert.containsText('h3', 'Log In')
+      .end()
+  },
+  'Sign In': function (browser) {
+    browser
+      .url('http://localhost:3000/login')
+      .pause(3000)
+      .setValue('input[name=username]', 'username')
+      .setValue('input[name=password]', 'password')
+      .click('input[type=submit]')
+      .pause(3000)
+      .assert.containsText('h2', 'Latest Submission')
+      .end()
+  }
+}
