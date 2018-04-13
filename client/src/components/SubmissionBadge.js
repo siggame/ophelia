57cab23db4583a562a3d53301e93715ed75b5c98
Reno DuBois <renodubois@users.noreply.github.com>
2018-04-13 14:05:18 -0500

    Fix grid spacing on submission badge

--- client/src/components/SubmissionBadge.js
+++ client/src/components/SubmissionBadge.js
@@ -50,9 +50,6 @@ export default class SubmissionBadge extends Component {
 
     const logIconVector = (
       <svg style={{ verticalAlign: 'middle', width: '8vh', height: '8vh' }} version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 485.212 485.212" xmlSpace="preserve">
-        {/* <g> */}
-          {/* <rect style={{ verticalAlign: 'middle', width: '30vh', height: '30vh', fill:'grey', fillOpacity: '1'}}/> */}
-        {/* </g> */}
         <g>
           <path style={{ fill: 'grey' }} d="M424.56,90.978h-90.976V0L424.56,90.978z M454.887,121.304v363.908H30.325V0h272.928v121.304H454.887z M90.976,121.304 h181.955V90.978H90.976V121.304z M394.234,363.906H90.976v30.331h303.258V363.906z M394.234,272.93H90.976v30.327h303.258V272.93z M394.234,181.955H90.976v30.327h303.258V181.955z"/>
         </g>
@@ -61,10 +58,10 @@ export default class SubmissionBadge extends Component {
 
     return (
       <div style={{ backgroundColor: bgColor, margin: 10, height: '10vh' }} className='row'>
-      <div className='col-xs-3 text-center' style={{ height: '100%' }}>
+      <div className='col-xs-2 text-center' style={{ height: '100%' }}>
         <div style={{ lineHeight: '10vh', fontSize: 26 }} >{this.props.version}</div>
       </div>
-      <div className='col-xs-6 text-center' style={{ height: '100%', overflow: 'hidden' }}>
+      <div className='col-xs-8 text-center' style={{ height: '100%', overflow: 'hidden' }}>
         <div style={{ lineHeight: '10vh' }}><span style={{ fontSize: 20 }}>{this.props.status}</span> <span style={{ fontStyle: 'italic' }}>{'(' + updatedTime + ')'}</span></div>
       </div>
       <div className='col-xs-2 text-center' style={{ height: '100%', lineHeight: '10vh' }} >
