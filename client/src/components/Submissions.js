fa2fd596b5d8758614d74215b51f64563d24f7a9
Reno DuBois <renodubois@users.noreply.github.com>
2018-04-13 14:04:39 -0500

    Rename submissions, fix errors

--- client/src/components/Submissions.js
+++ client/src/components/Submissions.js
@@ -1,12 +1,40 @@
 import { distanceInWords } from 'date-fns'
 import { inject, observer } from 'mobx-react'
 import React from 'react'
+import ReactPaginate from 'react-paginate'
 
-import ButtonRefresh from '../ButtonRefresh'
+import ButtonRefresh from './ButtonRefresh'
+import SubmissionBadge from './SubmissionBadge'
+
+class SubmissionList extends React.Component {
+  render () {
+    let submissions = this.props.submissions
+    let submissionsList
+    if (submissions) {
+      if (submissions.length === 0) {
+        submissionsList = (
+          <div className='text-center' style={{ paddingTop: '5vh', height: '130px', minWidth: '320px' }} >
+            You haven't uploaded any submissions yet.
+          </div>
+        )
+      } else {
+        submissionsList = submissions.map((data) => {
+          return <SubmissionBadge {...data} key={data.id} />
+        })
+      }
+    }
+
+    return (
+      <div>
+        {submissionsList}
+      </div>
+    )
+  }
+}
 
 @inject('submissionStore')
 @observer
-export default class SubmissionList extends React.Component {
+export default class Submissions extends React.Component {
   constructor (props) {
     super(props)
 
@@ -17,16 +45,17 @@ export default class SubmissionList extends React.Component {
     this.handlePageClick = this.handlePageClick.bind(this)
     this.handleRefresh = this.handleRefresh.bind(this)
   }
-  
+
   handlePageClick (data) {
     // So this library we're using (React Paginate) thinks an awesome idea to 0-index page numbers.
     // In order to combat that, we have to add one, and type out this comment so someone doesn't get confused.
     let newPage = data.selected + 1
-    // Grab our games for the next page
+    // Grab our submissions for the next page
     this.setState({
       currentPage: newPage
     })
-    this.props.gameStore.loadGames(newPage)
+    // TODO: Submissions don't have pagintation support as of yet
+    this.props.submissionStore.loadSubmissions()
   }
 
   handleRefresh () {
@@ -34,7 +63,7 @@ export default class SubmissionList extends React.Component {
     this.setState({
       currentPage: 1
     })
-    this.props.gameStore.makeDataStale()
+    this.props.submissionStore.makeDataStale()
   }
 
 
@@ -72,13 +101,12 @@ export default class SubmissionList extends React.Component {
           <div className='col-xs-2' style={{ padding: '3vh 0 4px 0'}}><ButtonRefresh buttonOnClick={this.handleRefresh} /></div>
         </div>
         <div className='row' style={{ margin: '0 10px 0 10px' }}>
-          <div className='col-xs-3 text-center' >Opponent Name</div>
-          <div className='col-xs-5 text-center' >Result</div>
           <div className='col-xs-2 text-center' >Version</div>
-          <div className='col-xs-2 text-center' >Viz Link</div>
+          <div className='col-xs-8 text-center' >Status</div>
+          <div className='col-xs-2 text-center' >Build Log</div>
         </div>
         <SubmissionList submissions={this.props.submissionStore.submissions}/>
-        {paginateSection}   
+        {paginateSection}
       </div>
     )
   }
