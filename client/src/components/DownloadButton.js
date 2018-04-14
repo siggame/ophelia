34d277d87cc9e59289a5b4dca58b598f5a2fc881
Michael Beaver <michaelbeaver64@gmail.com>
2018-04-14 10:20:59 -0500

    Download button now detects if it received text or binary and uses the appropriate file extension

--- client/src/components/DownloadButton.js
+++ client/src/components/DownloadButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 const fileDownload = require('js-file-download')
-
+const isBinaryFile = require('isbinaryfile')
 
 export default class DownloadButton extends React.Component {
 
@@ -9,7 +9,15 @@ export default class DownloadButton extends React.Component {
     return new Promise((resolve, reject) => {
       axios.get(url)
         .then((response) => {
-          return resolve(response.data)
+          const data = response.data
+          const buffer = new Buffer(data)
+          isBinaryFile(buffer, 10000, (err, result) => {
+            return resolve({
+              file: data,
+              isBinary: result
+            })
+          })
+          // const isBinary = /\%u0000/.test(data)
         }).catch((err) => {
         return reject(err)
       })
@@ -20,13 +28,23 @@ export default class DownloadButton extends React.Component {
 
     if ((/\.(gz|zip)$/i).test(this.props.url)) {
       return (
-        <a onClick={() => {
-        this.getFile(this.props.url).then((file) => {
-          fileDownload(file, 'filename.txt')
+        <div onClick={() => {
+        this.getFile(this.props.url).then((result) => {
+          const decompressedName = this.props.url.match(/.*\/(.*)\.gz/)[1]
+          const compressedName = this.props.url.match(/.*\/(.*\.gz)/)[1]
+          let file = result.file
+          if (result.isBinary) {
+            const array = new Uint8Array(file.length);
+            for (let i = 0; i < file.length; i++){
+              array[i] = file.charCodeAt(i);
+            }
+            file = new Blob([array], {type: 'application/gzip'});
+          }
+          fileDownload(file, result.isBinary ? compressedName : decompressedName )
         })
       }} style={{ fontWeight: 'bold' }}>
         {this.props.html}
-        </a>
+        </div>
       )
 
     }
