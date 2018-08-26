f670b2827ddc2ce1790e9b352d0690e4d33c4eb2
Eric Willard <emwwwc@mst.edu>
2018-08-26 16:37:06 -0500

    Wrote most of teams API endpoint and tested it

--- server/db/init.js
+++ server/db/init.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const teams = require('./users')
+const teams = require('./teams')
 const submissions = require('./submissions')
 const games = require('./games')
 const users = require('./users')
