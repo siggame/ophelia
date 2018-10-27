388cc574114c9458d15dfdb66605aa13235e388d
Dillon Hess <dnhc59@mst.edu>
2018-10-27 09:18:09 -0500

    Prod webpack changes

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -196,7 +196,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.js$/, /\.html$/, /\.json$/],
+            exclude: [/\.js$/, /\.html$/, /\.json$/, /\.sass$/, /\.scss$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]'
             }
