06ff1422d0a6195bd107c8d11171ae271d39d560
Dillon Hess <dnhc59@mst.edu>
2019-01-27 20:20:25 -0600

    Fixed check to see if user is on a team

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,24 +4,33 @@ import SubmissionContainer from '../../containers/SubmissionContainer'
 import StatsContainer from '../../containers/StatsContainer'
 import { inject } from 'mobx-react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 @inject('authStore')
 export default class Dashboard extends React.Component {
   constructor (props) {
     super(props)
+    this.state = { teamname: '' }
+  }
+  componentDidMount() {
+        // Work around because I couldn't call this.props.authStore.user.teamName
+        // This should be fixed in the future
+        axios.get(`${process.env.REACT_APP_API_URL}/users/${this.props.authStore.userId}`).then((response) => {
+          this.setState({
+              userTeamName: response.data.user.teamName
+          })
+      })
   }
 
   render () {
-    if(!this.props.authStore.teamName) {
-      return (
+    return (
+      <div>
+      {this.state.userTeamName === null ?
         <div>
           <h3>You need a team name in order to see the dashboard and upload code!</h3>
           <h4>Visit <Link to='/create-team'>teams</Link> in order to create a team or <Link to='/teams'>invites</Link> to join a team.</h4>
         </div>
-      )
-    }
-    else {
-      return (
+        :
         <div>
           <div className='row'>
             <div className='col-md-6'>
@@ -35,7 +44,8 @@ export default class Dashboard extends React.Component {
             </div>
           </div>
         </div>
-      )
+      }
+      </div>
+    )
     }
-  }
 }
