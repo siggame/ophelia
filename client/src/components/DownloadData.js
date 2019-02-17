015506efb5cb05bd280d50fd3bdaf8f40ec48e15
Dillon Hess <dnhc59@mst.edu>
2019-02-17 13:43:07 -0600

    Cleaned up console.logs

--- client/src/components/DownloadData.js
+++ client/src/components/DownloadData.js
@@ -5,19 +5,14 @@ const fileDownload = require('js-file-download')
 
 export default class DownloadData extends React.Component {
 	getFile(version) {
-		console.log("this got called")
 		var url = process.env.REACT_APP_API_URL + '/submissions/' + version;
-		console.log(url);
-		return new Promise((resolve, reject) => {
-			axios({
-				method:'get',
-				url: url,
-				responseType: 'arraybuffer'
-			}).then(function (response) {
-				console.log(response.data);
-				var blob = new Blob([response.data], {type: "application/zip"});
-				fileDownload(blob, "version_" + version + ".zip");
-			})
+		axios({
+			method:'get',
+			url: url,
+			responseType: 'arraybuffer'
+		}).then(function (response) {
+			var blob = new Blob([response.data], {type: "application/zip"});
+			fileDownload(blob, "version_" + version + ".zip");
 		})
 	}
 
